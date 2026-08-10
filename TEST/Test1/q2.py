a22d2fdaaef08725eecc6ea2afa65cb584d211f9
Abhay Bhad <abhaybhad29@gmail.com>
2026-08-10 22:29:42 +0530

    Delete TEST directory

--- TEST/Test1/q2.py
+++
@@ -1,7 +0,0 @@
-#Write a program to calculate simple interest based on Principal, Rate and Time
-#(SI = P*R*T/100)
-P = float(input("Enter the principle Amount : "))
-R = float(input("Enter the Rate of interest : "))
-T  = float(input("Enter the Time : "))
-Si = (P*R*T)/100
-print("Simple interest is : ",Si)
--- TEST/Test1/q3.py
+++
@@ -1,7 +0,0 @@
-#Write a program to accept distance in km and convert it into meters and
-#centimeters both.
-km = float(input("Enter the kilo Meter : "))
-m = km * 1000
-cm = km * 100000
-print("Distance in Meter : ",m)
-print("Distance in CentiMeter : ",cm)
